0ef0c5e871cb8d53e2f0f7b05f30dbea4175311f
Satou_Dev <bogdansf16@gmail.com>
2024-10-18 19:39:00 +0500

    Изменение скриптов дороги

--- Assets/Scripts/RoadPart.cs
+++ Assets/Scripts/RoadPart.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 [ExecuteAlways]
 public class RoadPart : MonoBehaviour
 {
+    private Road _roadParent;
+    
     private void Start()
     {
+        if (!_roadParent)
+        {
+            this.DestroyScript();
+        }
         Debug.Log("Initialized");
     }
-
+    
     public void DestroyScript()
     {
         Destroy(this);
