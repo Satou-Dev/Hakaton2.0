e34e20932e277dd8cf203302c841f8f718c692aa
Satou_Dev <bogdansf16@gmail.com>
2024-10-19 15:09:01 +0500

    ML

--- Assets/Scripts/CarAgent.cs
+++ Assets/Scripts/CarAgent.cs
@@ -18,32 +18,32 @@ public class CarAgent : Agent
     
     public override void OnEpisodeBegin()
     {
-        // Сброс состояния машины
         carRigidbody.linearVelocity = Vector3.zero;
         transform.localPosition = _startTransform.localPosition;
         transform.localRotation = _startTransform.localRotation;
     }
-
+    
     public override void CollectObservations(VectorSensor sensor)
     {
         // Добавляем векторные наблюдения (например, скорость машины)
         sensor.AddObservation(carRigidbody.linearVelocity.magnitude);
-        sensor.AddObservation(transform.forward);
+        // sensor.AddObservation(transform.forward);
     }
-
+    
     public override void OnActionReceived(ActionBuffers actions)
     {
-        // Управляем машиной с помощью действий
         float steer = actions.ContinuousActions[0]; // Поворот
         float speed = actions.ContinuousActions[1]; // Скорость
 
         // Применение поворота и ускорения
         wheels[0].steerAngle = 30f * steer;
         wheels[1].steerAngle = 30f * steer;
+        
+        Debug.Log(steer + "  " + speed);
 
         foreach (WheelCollider wheel in wheels)
         {
-            wheel.motorTorque = 1000f * speed;
+            wheel.motorTorque = 5f * speed;
         }
     }
 }
