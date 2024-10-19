cdb21ce7c464abee2f3f5a0c82ea843dfbe2b4af
Satou_Dev <bogdansf16@gmail.com>
2024-10-19 22:21:49 +0500

    Логика апдейт

--- Assets/Scripts/CarAgent.cs
+++ Assets/Scripts/CarAgent.cs
@@ -9,18 +9,23 @@ public class CarAgent : Agent
     public Rigidbody carRigidbody;
     public WheelCollider[] wheels;
     
-    private Transform _startTransform;
+    public Vector3 startPosition;
+    public Vector3 startRotation;
     
+    private Transform _startTransform;
+
     private void Start()
     {
-        _startTransform = this.transform;
+        startPosition = this.transform.position;
+        startRotation = this.transform.rotation.eulerAngles;
     }
-    
+
     public override void OnEpisodeBegin()
     {
+        Debug.Log("Reset | " + GetCumulativeReward());
         carRigidbody.linearVelocity = Vector3.zero;
-        transform.localPosition = _startTransform.localPosition;
-        transform.localRotation = _startTransform.localRotation;
+        transform.localPosition = startPosition;
+        transform.localRotation = Quaternion.Euler(startRotation);
     }
     
     public override void CollectObservations(VectorSensor sensor)
@@ -34,16 +39,47 @@ public class CarAgent : Agent
     {
         float steer = actions.ContinuousActions[0]; // Поворот
         float speed = actions.ContinuousActions[1]; // Скорость
-
+        
         // Применение поворота и ускорения
         wheels[0].steerAngle = 30f * steer;
         wheels[1].steerAngle = 30f * steer;
-        
-        Debug.Log(steer + "  " + speed);
 
         foreach (WheelCollider wheel in wheels)
         {
             wheel.motorTorque = 5f * speed;
         }
+
+        float forwardMove = Vector3.Dot(Vector3.Normalize(carRigidbody.linearVelocity), transform.forward);
+        if (forwardMove <= 0)
+        {
+            AddReward(-0.001f);
+        }
+        if (forwardMove >= 0.5)
+        {
+            AddReward(0.005f * speed);
+        }
+        AddReward(-0.001f);
+
+        if (transform.position.y <= -50)
+        {
+            AddReward(-10f);
+            EndEpisode();
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Track"))
+        {
+            AddReward(-1f); // Штраф за столкновение с препятствием
+            AddReward(-1f * carRigidbody.linearVelocity.magnitude); // Штраф за неторможение перед препядствием при столкновении
+            EndEpisode();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        AddReward(-1f);
+        EndEpisode();
     }
 }
